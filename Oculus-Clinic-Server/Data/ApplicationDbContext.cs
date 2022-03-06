b7964d9b1af8a660f67b1fc9f6620b1fe833dcfc
chermen <dzheriev2003@mail.ru>
2022-03-06 11:40:41 +0300

    Add connection to SQL

---
+++ Oculus-Clinic-Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oculus_Clinic_Server.Data.Models;
+
+namespace Oculus_Clinic_Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+
+    } 
+}
--- Oculus-Clinic-Server/Program.cs
+++ Oculus-Clinic-Server/Program.cs
@@ -1,14 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Oculus_Clinic_Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Database")), ServiceLifetime.Singleton);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
